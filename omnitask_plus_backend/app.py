030b60103f39cfe0d09e72988a9c93b5355825a4
ProvLeon <providence.leonard@gmail.com>
2024-03-29 20:17:43 +0000

    update againn

--- omnitask_plus_backend/app.py
+++ omnitask_plus_backend/app.py
@@ -6,40 +6,40 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from database import init_db
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+app = Flask(__name__)
 
-    app.config['BASE_URL'] = 'http://localhost:5000'
+app.config['BASE_URL'] = 'http://localhost:5000'
 
-    # Generate a random secret key
-    app.config['JWT_SECRET_KEY'] = os.urandom(24).hex()
-    jwt = JWTManager(app)
+# Generate a random secret key
+app.config['JWT_SECRET_KEY'] = os.urandom(24).hex()
+jwt = JWTManager(app)
 
-    # Initialize CORS
-    CORS(app)
+# Initialize CORS
+CORS(app)
 
-    @app.before_request
-    def initialize_database():
-        init_db()
+@app.before_request
+def initialize_database():
+    init_db()
 
-    # Import and register blueprints inside the factory function
-    from routes.user_routes import bp as user_routes_bp
-    from routes.task_routes import bp as task_routes_bp
-    from routes.study_sessions_routes import bp as study_sessions_routes_bp
-    from routes.login import bp as login_bp
-    from routes.files_route import bp as files_route_bp
-    app.register_blueprint(user_routes_bp)
-    app.register_blueprint(task_routes_bp)
-    app.register_blueprint(study_sessions_routes_bp)
-    app.register_blueprint(login_bp)
-    app.register_blueprint(files_route_bp)
+# Import and register blueprints inside the factory function
+from routes.user_routes import bp as user_routes_bp
+from routes.task_routes import bp as task_routes_bp
+from routes.study_sessions_routes import bp as study_sessions_routes_bp
+from routes.login import bp as login_bp
+from routes.files_route import bp as files_route_bp
+app.register_blueprint(user_routes_bp)
+app.register_blueprint(task_routes_bp)
+app.register_blueprint(study_sessions_routes_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(files_route_bp)
 
 
-    # return app
-    @jwt.expired_token_loader
-    def my_expired_token_callback(jwt_header, jwt_payload):
-        return jsonify({"error": "Token has expired"}), 401
+# return app
+@jwt.expired_token_loader
+def my_expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({"error": "Token has expired"}), 401
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='127.0.0.1', port=5000)
+    # app = create_app()
+    app.run()
